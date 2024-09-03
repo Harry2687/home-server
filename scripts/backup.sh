107b8e391759c54c25987ceec60fb858d8b720d0
Harry Zhong <harry.zhong2687@gmail.com>
2024-09-04 06:55:56 +0800

    updated backup.sh and added more ram to mc server

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # Define variables
+DOCKERDIR="/home/harryzhong/docker"
 RESTIC_PASSWORD="/home/harryzhong/restic_password"
 BACKUP_SOURCE="/home/harryzhong/docker/appdata"
 BACKUP_REPO_REMOTE="rclone:gdrive:Backups/optiplexmediaserver"
 BACKUP_REPO_LOCAL="/media/hd1/backups/appdata"
-KEEP_OPTIONS="--keep-daily 6 --keep-weekly 3 --keep-monthly 1 --keep-yearly 1"
+KEEP_OPTIONS="--keep-daily 7 --keep-weekly 4 --keep-monthly 3"
 
 (
+  cd $DOCKERDIR
+  
   # Stop Docker containers
   docker compose down
 
   # Backup to local repo
-  restic -p $RESTIC_PASSWORD -r $BACKUP_REPO_LOCAL --verbose backup $BACKUP_SOURCE 
+  restic -p $RESTIC_PASSWORD -r $BACKUP_REPO_LOCAL --verbose backup $BACKUP_SOURCE
 
   # Prune local repo
   restic -p $RESTIC_PASSWORD -r $BACKUP_REPO_LOCAL --verbose forget $KEEP_OPTIONS --prune --cleanup-cache 
 
   # Backup to remote repo
-  restic -p $RESTIC_PASSWORD -r $BACKUP_REPO_REMOTE --verbose backup $BACKUP_SOURCE 
+  restic -p $RESTIC_PASSWORD -r $BACKUP_REPO_REMOTE --verbose backup $BACKUP_SOURCE
 
   # Prune remote repo
   restic -p $RESTIC_PASSWORD -r $BACKUP_REPO_REMOTE --verbose forget $KEEP_OPTIONS --prune --cleanup-cache 
