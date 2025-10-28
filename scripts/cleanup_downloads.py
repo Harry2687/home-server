024fd2a4fa8bc577417db60a975b7ad4a2da9c99
Harry Zhong <harry.zhong2687@gmail.com>
2025-10-28 21:33:13 +0800

    add logic to delete files/directories and add option for user to confirm

--- scripts/cleanup_downloads.py
+++ scripts/cleanup_downloads.py
@@ -14,16 +14,57 @@ import qbittorrentapi
 
 load_dotenv()
 
-downloads_directory = "/media/storage/arr_data/downloads"
+
+def confirm(message: str) -> None:
+    confirmation = input(message).strip().lower()
+    if confirmation in ("yes", "y"):
+        return True
+    elif confirmation in ("no", "n"):
+        return False
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
 
 
-def main() -> None:
-    qbt_client = qbittorrentapi.Client(
-        host="localhost:8080",
-        username=os.getenv("QBIT_USER"),
-        password=os.getenv("QBIT_PWD"),
-    )
+def delete_file_or_dir(path: str) -> None:
+    if os.path.isdir(path):
+        os.rmdir(path)
+    elif os.path.isfile(path):
+        os.remove(path)
 
+
+def delete_file_or_dir_with_confirm(
+    path: str, message: str, show_confirm: bool = True
+) -> None:
+    if show_confirm:
+        if confirm(message):
+            delete_file_or_dir(path)
+    else:
+        delete_file_or_dir(path)
+
+
+def delete_torrent_with_confirm(
+    client: qbittorrentapi.Client,
+    hash: str,
+    message: str,
+    downloads_directory: str,
+    show_confirm: bool = True,
+) -> None:
+    file_or_folder_name = client.torrents_files(hash)[0].name.split("/")[0]
+    file_or_folder_path = f"{downloads_directory}/{file_or_folder_name}"
+    if show_confirm:
+        if confirm(message):
+            client.torrents_delete(delete_files=False, torrent_hashes=hash)
+            delete_file_or_dir(file_or_folder_path)
+    else:
+        client.torrents_delete(delete_files=False, torrent_hashes=hash)
+        delete_file_or_dir(file_or_folder_path)
+
+
+def main(
+    qbt_client: qbittorrentapi.Client,
+    downloads_directory: str,
+    show_confirm: bool = True,
+) -> None:
     try:
         qbt_client.auth_log_in()
     except qbittorrentapi.LoginFailed as e:
@@ -31,30 +72,47 @@ def main() -> None:
 
     seeding_torrents = qbt_client.torrents_info(status_filter="seeding")
     all_torrents = qbt_client.torrents_info()
-    all_files = os.listdir(downloads_directory)
-    all_torrent_files = []
+    all_files_and_dirs = os.listdir(downloads_directory)
+    all_torrent_files_and_dirs = []
 
     # Identify directories/files which are not registered as a torrent
     for torrent in all_torrents:
         files = qbt_client.torrents_files(torrent.hash)
         if torrent.state != "metaDL":
             file_or_folder_name = files[0].name.split("/")[0]
-            all_torrent_files.append(file_or_folder_name)
+            all_torrent_files_and_dirs.append(file_or_folder_name)
 
-    not_torrent_files = list(set(all_files) - set(all_torrent_files))
-    for file in not_torrent_files:
-        print(f"Not a torrent: {file}")
+    not_torrent_files_and_dirs = list(
+        set(all_files_and_dirs) - set(all_torrent_files_and_dirs)
+    )
+    if len(not_torrent_files_and_dirs) > 0:
+        for entry in not_torrent_files_and_dirs:
+            delete_file_or_dir_with_confirm(
+                path=f"{downloads_directory}/{entry}",
+                message=f"Not a torrent: {entry} | Delete? (yes/no): ",
+                show_confirm=show_confirm,
+            )
+            print(f"Deleted file/directory (not a torrent): {entry}")
+    else:
+        print("All files and directories are linked to a torrent.")
 
     # Identify torrents which are seeding but not hardlinked
+    n_torrents_with_no_hardlinks = 0
     for torrent in seeding_torrents:
         files = qbt_client.torrents_files(torrent.hash)
         if len(files) == 1 and files[0].name.count("/") == 0:
             file_name = files[0].name
             stat_info = os.stat(f"{downloads_directory}/{file_name}")
             if stat_info.st_nlink == 1:
-                print(f"Not a hard link: {file_name}")
-            else:
-                print(f"Hard link: {file_name}")
+                delete_torrent_with_confirm(
+                    client=qbt_client,
+                    hash=torrent.hash,
+                    downloads_directory=downloads_directory,
+                    message=f"Not a hard link: {file_name} | Delete? (yes/no): ",
+                    show_confirm=show_confirm,
+                )
+                print(f"Deleted file (not hard linked): {file_name}")
+                n_torrents_with_no_hardlinks += 1
         else:
             folder_name = files[0].name.split("/")[0]
             has_hardlink = False
@@ -64,15 +122,29 @@ def main() -> None:
                     full_file_path = os.path.join(root, file_name)
                     stat_info = os.stat(full_file_path)
                     if stat_info.st_nlink > 1:
-                        hardlinked_file = file_name
                         has_hardlink = True
                         break
 
             if not has_hardlink:
-                print(f"No hard links: {folder_name}")
-            else:
-                print(f"Contains a hard link: {hardlinked_file}")
+                delete_torrent_with_confirm(
+                    client=qbt_client,
+                    hash=torrent.hash,
+                    downloads_directory=downloads_directory,
+                    message=f"Does not contain hard links: {folder_name} | Delete? (yes/no): ",
+                    show_confirm=show_confirm,
+                )
+                print(f"Deleted directory (no hard links): {folder_name}")
+                n_torrents_with_no_hardlinks += 1
+
+    if n_torrents_with_no_hardlinks == 0:
+        print("All torrents contain at least 1 hard link.")
 
 
 if __name__ == "__main__":
-    main()
+    downloads_directory = "/media/storage/arr_data/downloads"
+    qbt_client = qbittorrentapi.Client(
+        host="localhost:8080",
+        username=os.getenv("QBIT_USER"),
+        password=os.getenv("QBIT_PWD"),
+    )
+    main(qbt_client=qbt_client, downloads_directory=downloads_directory)
